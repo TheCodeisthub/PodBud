b1aeb53646f94811e88d1902466147f353fcfb1e
TheCodeisthub <howdy.roosevelt@gmail.com>
2019-09-27 07:57:17 +0100

    add homepage and navigations

--- src/layout/topbar/Topbar.jsx
+++ src/layout/topbar/Topbar.jsx
@@ -1,12 +1,19 @@
 import React, { Component } from "react";
+import userAvatar from "../../images/chris.png";
+
+import "./topbar.scss";
 
 export default class Topbar extends Component {
   render() {
     return (
       <div className="db__header">
-        <form>
+        <form className="topbar--form">
           <input type="search" name="" id="" placeholder="Search" />
         </form>
+        <div>
+          <div className="notify"></div>
+          <img className="avatar" src={userAvatar} alt="avatar" />
+        </div>
       </div>
     );
   }
--- src/pages/admin/home/Home.jsx
+++ src/pages/admin/home/Home.jsx
@@ -1,11 +1,33 @@
 import React, { Component } from "react";
 import Layout from "../../../layout";
 
+import "./home.scss";
+
 export default class Home extends Component {
   render() {
     return (
       <Layout>
-        <h2>Home Dashbosrd</h2>
+        <section className="card--container">
+          <div className="podcast--card">
+            <div className="card--content">
+              <h2>BUILDING BRAND LOYALTY</h2>
+              <p>Wes McDowell</p>
+              <span>23.05.2019</span>
+              <span>24:15:05</span>
+              <div className="play--btn--red"></div>
+            </div>
+          </div>
+
+          <div className="podcast--card">
+            <div className="card--content">
+              <h2>BUILDING BRAND LOYALTY</h2>
+              <p>Wes McDowell</p>
+              <span>23.05.2019</span>
+              <span>24:15:05</span>
+              <div className="play--btn--red"></div>
+            </div>
+          </div>
+        </section>
       </Layout>
     );
   }
